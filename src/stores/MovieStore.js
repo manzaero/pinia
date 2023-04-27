a16293acb836950248f64d1f2b02d8aee4069d00
manzaero <ligoero@gmail.com>
2023-04-27 08:55:21 +0500

    refact option api in composition api success

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -1,28 +1,62 @@
 import {defineStore} from 'pinia';
+import {ref, computed} from "vue";
 
-export const useMovieStore = defineStore("movieStore", {
-    state: () => ({
-        movies:[],
-        activeTab: 2,
-    }),
-    getters: {
-        watchedMovies(){
-            return this.movies.filter((el)=> el.isWatched === true)
-        },
-        totalCount(){
-            return this.movies.length
-        }
-    },
-    actions:{
-      setActive(id){
-          this.activeTab = id
-      },
-        toggleMovie(id){
-            const index = this.movies.findIndex(el => el.id === id)
-            this.movies[index].isWatched = !this.movies[index].isWatched
-        },
-        deleteMovie(id){
-          this.movies = this.movies .filter(el => el.id !== id)
-        }
+// export const useMovieStore = defineStore("movieStore", {
+//     state: () => ({
+//         movies:[],
+//         activeTab: 2
+//     }),
+//     getters: {
+//         watchedMovies(){
+//             return this.movies.filter((el)=> el.isWatched)
+//         },
+//         totalCount(){
+//             return this.movies.length
+//         }
+//     },
+//     actions:{
+//       setActive(id){
+//           this.activeTab = id
+//       },
+//     toggleMovie(id){
+//             const index = this.movies.findIndex((el) => el.id === id)
+//             this.movies[index].isWatched = !this.movies[index].isWatched
+//         },
+//     deleteMovie(id){
+//           this.movies = this.movies.filter((el) => el.id !== id);
+//         }
+//     }
+// });
+
+export const useMovieStore = defineStore("movieStore", () => {
+    const movies = ref([]);
+    const activeTab = ref(2)
+
+    const watchedMovies = computed(() => {
+        return movies.value.filter((el)=> el.isWatched)
+    })
+
+    const totalCount = computed(() => {
+        return movies.value.length
+    })
+
+    const setActive = (id) => {
+        activeTab.value = id
     }
-});
+    const toggleMovie = (id) => {
+        const index = movies.value.findIndex((el) => el.id === id)
+        movies.value[index].isWatched = !movies.value[index].isWatched
+    }
+    const deleteMovie = (id) => {
+        movies.value = movies.value.filter((el) => el.id !== id);
+    }
+    return {
+        movies,
+        activeTab,
+        watchedMovies,
+        totalCount,
+        setActive,
+        toggleMovie,
+        deleteMovie
+    }
+})
--- src/stores/SearchStore.js
+++ src/stores/SearchStore.js
@@ -1,26 +1,32 @@
 import { defineStore } from "pinia";
+import {ref} from "vue";
 import {useMovieStore} from "./MovieStore.js";
 import axios from "axios";
 
 const url = 'https://api.themoviedb.org/3/search/movie?api_key=18fd9b8cef6e0d8cbb9e5efdf65774ec&query=';
 
-export const useSearchStore = defineStore("searchStore", {
-    state: () => ({
-        loader: false,
-        movies: []
-    }),
-    actions: {
-        async getMovie(search) {
-            this.loader = true;
-            let res = await axios(`${url}${search}`)
-            console.log(res.data)
-            this.movies = res.data.results
-            this.loader = false
-        },
-        addToMovies(object){
-            const movieStore = useMovieStore();
-            movieStore.movies.push(object)
-            console.log(object)
-        }
+export const useSearchStore = defineStore("searchStore", () => {
+    const loader = ref(false);
+    const movies = ref([]);
+
+    const getMovie = async(search) => {
+        loader.value = true;
+        let res = await axios(`${url}${search}`)
+        console.log(res.data)
+        movies.value = res.data.results
+        loader.value = false
+    };
+
+    const addToMovies = (object) => {
+        const movieStore = useMovieStore();
+        movieStore.movies.push({...object, isWatched: false});
+        setTimeout(()=> movieStore.activeTab = 1,500)
+        console.log(object)
+    }
+    return {
+        loader,
+        movies,
+        getMovie,
+        addToMovies
     }
 })
